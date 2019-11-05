acf0e20908edbe2aa1db20ab2a578d16bc28a26f
Tom Peres <tomp@wix.com>
2019-11-05 19:24:11 +0200

    ssr state leaks example

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import {Component} from 'svelte-app-components';
+  import Wat from './Wat.svelte';
   let name = 'tomp';
   let _innerWidth;
 </script>
@@ -8,3 +9,4 @@
 <h1>Helllllo</h1>
 <h2>I am in {typeof _innerWidth === 'number' ? 'client' : 'ssr'}</h2>
 <Component name={name}></Component>
+<Wat></Wat>
---
+++ svelte-app/src/Wat.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+  let count = 0
+</script>
+
+<script>
+  count = count + 1;
+</script>
+
+<h3>🚀 {count}</h3>
